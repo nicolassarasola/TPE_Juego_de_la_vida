af742bfd90c0439b9d580214301d96ecaac08ce3
Nicolas <134214817+nicolassarasola@users.noreply.github.com>
2026-04-03 13:19:57 -0300

    añadidos comentarios a Estado

--- estados/Estado.java
+++ estados/Estado.java
@@ -3,10 +3,13 @@ package estados;
 
 // define el comportamiento de cada estado posible de una celda
 public abstract class Estado {
-
+    //devuelve su simbolo
     public abstract char getCaracter();
+    //devuelve su nombre
     public abstract String getNombre();
+    //retorna true si se considera viva
     public abstract boolean esViva();
+    //considera cual es su nuevo estado dependiendo de los vecinos vivos
     public abstract Estado comprobarEstadoNuevo(int vecinosVivos);
 
 
